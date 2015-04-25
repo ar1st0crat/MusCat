9a121f1aec16c125697613af1c04b98b85ea5e28
ar1st0crat <art1st.prog@gmail.com>
2015-04-26 00:46:17 +0300

    fixed some bug. Wrote some TODOs

--- MusCatalog/RadioPlayerWindow.xaml.cs
+++ MusCatalog/RadioPlayerWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NAudio.Wave;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -22,23 +23,32 @@ namespace MusCatalog
     public partial class RadioPlayerWindow : Window
     {
         List<Songs> songs = new List<Songs>();
+        int nCurrentSong = 1;
 
-        private void ShowSongs()
+        BitmapImage imagePlay = new BitmapImage(new Uri(@"Images\play.png", UriKind.Relative));
+        BitmapImage imagePause = new BitmapImage(new Uri(@"Images\pause.png", UriKind.Relative));
+
+        WaveOut waveOut = new WaveOut();
+        
+        bool bPlaying = false;
+
+
+        private void UpdateSongs()
         {
-            if (songs[0] != null)
+            if (songs[nCurrentSong - 1].SID != -1 )
             {
-                ((DockPanel)this.prevSongPanel.FindName("prevSong")).DataContext = songs[0];
-                this.prevSongPanel.DataContext = songs[0].Albums;
-                this.prevImage.DataContext = songs[0].Albums;
+                ((DockPanel)this.prevSongPanel.FindName("prevSong")).DataContext = songs[nCurrentSong - 1];
+                this.prevSongPanel.DataContext = songs[nCurrentSong - 1].Albums;
+                this.prevImage.DataContext = songs[nCurrentSong - 1].Albums;
             }
 
-            ((DockPanel)this.curSongPanel.FindName("curSong")).DataContext = songs[1];
-            this.curImage.DataContext = songs[1].Albums;
-            this.curSongPanel.DataContext = songs[1].Albums;
+            ((DockPanel)this.curSongPanel.FindName("curSong")).DataContext = songs[nCurrentSong];
+            this.curImage.DataContext = songs[nCurrentSong].Albums;
+            this.curSongPanel.DataContext = songs[nCurrentSong].Albums;
 
-            ((DockPanel)this.nextSongPanel.FindName("nextSong")).DataContext = songs[2];
-            this.nextSongPanel.DataContext = songs[2].Albums;
-            this.nextImage.DataContext = songs[2].Albums;
+            ((DockPanel)this.nextSongPanel.FindName("nextSong")).DataContext = songs[nCurrentSong + 1];
+            this.nextSongPanel.DataContext = songs[nCurrentSong + 1].Albums;
+            this.nextImage.DataContext = songs[nCurrentSong + 1].Albums;
 
 
             StartPlayingCurrentSong();
@@ -54,31 +64,51 @@ namespace MusCatalog
                 Random songSelector = new Random();
 
                 var maxSID = context.Songs.Max( s => s.SID );
-                var songNo = songSelector.Next() % maxSID;
-                var selectedsongs = from s in context.Songs
-                       where s.SID >= songNo
-                       select s;
 
-                song = selectedsongs.First();
-                song.Albums = (from a in context.Albums
-                               where a.AID == song.AID
-                               select a).
-                               First();
-                song.Albums.Performers = (from p in context.Performers 
-                                          where p.PID == song.Albums.Performers.PID 
-                                          select p).
-                                          First();
+                do
+                {
+                    var songNo = songSelector.Next() % maxSID;
+                    var selectedsongs = from s in context.Songs
+                                        where s.SID >= songNo
+                                        select s;
+
+                    if (this.ShortSongs.IsChecked.Value)
+                        selectedsongs = selectedsongs.Where(s => s.STime.Length <= 4 && s.STime.CompareTo("3:00") < 0);
+
+                    song = selectedsongs.First();
+                    song.Albums = (from a in context.Albums
+                                   where a.AID == song.AID
+                                   select a).First();
+
+                    song.Albums.Performers = (from p in context.Performers
+                                              where p.PID == song.Albums.Performers.PID
+                                              select p).First();
+                }
+                while (songs.Where(s => s.SID == song.SID).Count() > 0); // && while exists file && );
             }
 
             return song;
         }
 
 
+        private void NextSong()
+        {
+            nCurrentSong++;
+
+            var s = SelectRandomSong();
+            this.nextImage.DataContext = s.Albums;
+            this.nextSongPanel.DataContext = s;
+            songs.Add(s);
+
+            UpdateSongs();
+        }
+        
+
         public RadioPlayerWindow()
         {
             InitializeComponent();
 
-            songs.Add( null );
+            songs.Add(new Songs { SID = -1, AID = -1, PID = -1, Albums = null, SRate = 0, SName = "", STime = "", SInfo ="", SN = 0 } );
 
             var s1 = SelectRandomSong();
             ((DockPanel)this.curSongPanel.FindName("curSong")).DataContext = s1;
@@ -91,121 +121,124 @@ namespace MusCatalog
             this.nextSongPanel.DataContext = s2;
             songs.Add(s2);
 
-            ShowSongs();
+            UpdateSongs();
         }
 
         private void NextMouseDown(object sender, MouseButtonEventArgs e)
         {
-            songs.RemoveAt(0);
-
-            var s = SelectRandomSong();
-            this.nextImage.DataContext = s.Albums;
-            this.nextSongPanel.DataContext = s;
-            songs.Add(s);
-
-            ShowSongs();
+            NextSong();
         }
 
         private void PrevMouseDown(object sender, MouseButtonEventArgs e)
         {
-            songs.RemoveAt(2);
-            songs.Insert(0, null );
+            if ( nCurrentSong > 1 )
+                nCurrentSong--;
 
-            ShowSongs();
+            UpdateSongs();
         }
 
         private void RadioPlayerKeyDown(object sender, KeyEventArgs e)
         {
-            if (e.Key != Key.Right)
-                return;
-
-            songs.RemoveAt(0);
-
-            var s = SelectRandomSong();
-            this.nextImage.DataContext = s.Albums;
-            songs.Add(s);
-
-            ShowSongs();
+            if (e.Key == Key.Right)
+                NextSong();
+            else if (e.Key == Key.Left)
+                PrevMouseDown( this, null );
         }
 
 
+        private string FindSongPath()
+        {
+            List<string> pathlist = new List<string>();
+            pathlist.Add( @"F:\" );
+            pathlist.Add( @"G:\" );
+            pathlist.Add( @"G:\Other\" );
 
-        MediaElement mp3song = new MediaElement();
-        bool bPlaying = false;
+            foreach (var rootpath in pathlist)
+            {
+                string pathDir = rootpath +
+                                    songs[nCurrentSong].Albums.Performers.Performer[0] +
+                                    System.IO.Path.DirectorySeparatorChar +
+                                    songs[nCurrentSong].Albums.Performers.Performer +
+                                    System.IO.Path.DirectorySeparatorChar;
 
-        BitmapImage imagePlay = new BitmapImage(new Uri(@"Images\play.png", UriKind.Relative));
-        BitmapImage imagePause = new BitmapImage(new Uri(@"Images\pause.png", UriKind.Relative));
+                if (Directory.Exists(pathDir))
+                {
+                    // TODO:
+                    // check if song name contains punctuation and eliminate it! Normalize string - remove spaces and punctuation!
+                    //
+                    string[] dirs = Directory.GetDirectories(pathDir);
+                    var neededDirs = dirs.Where(d => d.Contains(songs[nCurrentSong].Albums.AYear.ToString()) && d.Contains(songs[nCurrentSong].Albums.Album));
 
+                    if (neededDirs.Count() > 0)
+                        return neededDirs.First();
+                }
+            }
+
+            return "";
+        }
 
         private void StartPlayingCurrentSong()
         {
             if (bPlaying)
-                mp3song.Stop();
+            {
+                waveOut.Stop();
+                waveOut.Dispose();
+            }
 
-            string pathDir = @"F:\" +
-                                    songs[1].Albums.Performers.Performer[0] +
-                                    System.IO.Path.DirectorySeparatorChar +
-                                    songs[1].Albums.Performers.Performer +
-                                    System.IO.Path.DirectorySeparatorChar +
-                                    songs[1].Albums.AYear + " - " + songs[1].Albums.Album;
+            string pathDir = FindSongPath();
             
-            if (!Directory.Exists(pathDir) || songs[1].SN >= Directory.GetFiles( pathDir ).Length )
+            if ( pathDir == "" )
             {
                 NextMouseDown(this, null);
             }
             else
             {
-                MessageBox.Show(songs[1].SN.ToString());
-                //MessageBox.Show( pathDir );
-                //MessageBox.Show(Directory.GetFiles(pathDir)[songs[1].SN - 1]);
+                Mp3FileReader mp3Reader = new Mp3FileReader( Directory.GetFiles(pathDir)[songs[nCurrentSong].SN - 1] );
 
-                mp3song.Source = new Uri(Directory.GetFiles(pathDir)[songs[1].SN - 1] );
-                mp3song.LoadedBehavior = MediaState.Manual;
-                mp3song.UnloadedBehavior = MediaState.Manual;
-
-                mp3song.MediaEnded += mp3song_MediaEnded;
-
-                mp3song.Play();
+                waveOut = new WaveOut();
+                waveOut.Init(mp3Reader);
+                waveOut.Play();
+                waveOut.PlaybackStopped += SongPlaybackStopped;
+                
                 bPlaying = true;
             }
+        }
+
+        private void SongPlaybackStopped(object sender, StoppedEventArgs e)
+        {
+            waveOut.Stop();
+            bPlaying = false;
+            NextSong();
         }
 
         private void PausePlayMouseDown(object sender, MouseButtonEventArgs e)
         {
             if (bPlaying)
             {
-                mp3song.Pause();
+                waveOut.Pause();
                 bPlaying = false;
                 this.playback.Source = imagePlay;
             }
             else
             {
-                mp3song.Play();
+                waveOut.Play();
                 bPlaying = true;
                 this.playback.Source = imagePause;
             }
         }
 
-        private void mp3song_MediaEnded(object sender, RoutedEventArgs e)
-        {
-            songs.RemoveAt(0);
-
-            var s = SelectRandomSong();
-            this.nextImage.DataContext = s.Albums;
-            this.nextSongPanel.DataContext = s;
-            songs.Add(s);
-
-            ShowSongs();
-        }
 
         private void StopMouseDown(object sender, MouseButtonEventArgs e)
         {
-            mp3song.Stop();
+            waveOut.Stop();
+            bPlaying = false;
+            this.playback.Source = imagePlay;
         }
+
 
         private void SliderVolumeChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            mp3song.Volume = this.VolumeSlider.Value / 10.0;
+            waveOut.Volume = (float)(this.VolumeSlider.Value / 10.0);
         }
     }
 }
